69264fb5cb75d9d27008fd87744bae96a7e2d785
Colin Sygiel <ctsygiel@gmail.com>
2019-08-15 23:43:57 -0700

    header styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,49 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import { Div, Container, Row, Anchor } from "atomize"
 
 const Header = ({ siteTitle }) => (
-  <header
+  <Div
     style={{
-      background: `rebeccapurple`,
+      background: `white`,
       marginBottom: `1.45rem`,
     }}
   >
-    <div
+    <Row
+      d="flex"
+      align="center"
       style={{
         margin: `0 auto`,
         maxWidth: 1760,
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+      <Div p={{ r: "3rem" }}>
+        <h1 style={{ margin: 0 }}>
+          <Link
+            to="/"
+            style={{
+              color: `black`,
+              textDecoration: `none`,
+            }}
+          >
+            {siteTitle}
+          </Link>
+        </h1>
+      </Div>
+
+      <Anchor p={{ l: "1rem" }} href="https://www.google.com" d="block">
+        Link 1
+      </Anchor>
+      <Anchor p={{ l: "1rem" }} href="https://www.google.com" d="block">
+        Link 2
+      </Anchor>
+      <Anchor p={{ l: "1rem" }} href="https://www.google.com" d="block">
+        Link 3
+      </Anchor>
+    </Row>
+  </Div>
 )
 
 Header.propTypes = {
