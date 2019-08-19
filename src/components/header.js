398c2ce89356b6478136e6b5893787bb7b0fdde9
Colin Sygiel <ctsygiel@gmail.com>
2019-08-18 23:08:04 -0700

    added landing video and dynamic styled header

--- src/components/header.js
+++ src/components/header.js
@@ -1,50 +1,116 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Div, Container, Row, Anchor } from "atomize"
 
-const Header = ({ siteTitle }) => (
-  <Div
-    style={{
-      background: `white`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <Row
-      d="flex"
-      align="center"
-      style={{
-        margin: `0 auto`,
-        maxWidth: 1760,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <Div p={{ r: "3rem" }}>
-        <h1 style={{ margin: 0 }}>
-          <Link
-            to="/"
-            style={{
-              color: `black`,
-              textDecoration: `none`,
-            }}
-          >
-            {siteTitle}
-          </Link>
-        </h1>
-      </Div>
+function Header({ siteTitle }) {
+  const [isTop, setIsTop] = useState(true)
 
-      <Anchor p={{ l: "1rem" }} href="https://www.google.com" d="block">
-        Link 1
-      </Anchor>
-      <Anchor p={{ l: "1rem" }} href="https://www.google.com" d="block">
-        Link 2
-      </Anchor>
-      <Anchor p={{ l: "1rem" }} href="https://www.google.com" d="block">
-        Link 3
-      </Anchor>
-    </Row>
-  </Div>
-)
+  useEffect(() => {
+    document.addEventListener("scroll", () => {
+      const isTopCheck = window.scrollY < 1
+      if (isTopCheck !== isTop) {
+        setIsTop(isTopCheck)
+      }
+      console.log(isTop)
+    })
+  })
+
+  if (isTop) {
+    return (
+      <Div
+        style={{
+          zIndex: 5000,
+          background: `white`,
+          marginBottom: `1.45rem`,
+          width: "100%",
+          top: 0,
+          position: "fixed",
+        }}
+      >
+        <Row
+          d="flex"
+          align="center"
+          style={{
+            margin: `0 auto`,
+            maxWidth: 1760,
+            padding: `1.45rem 1.0875rem`,
+          }}
+        >
+          <Div p={{ r: "3rem" }}>
+            <h1 style={{ margin: 0 }}>
+              <Link
+                to="/"
+                style={{
+                  color: `black`,
+                  textDecoration: `none`,
+                }}
+              >
+                {siteTitle}
+              </Link>
+            </h1>
+          </Div>
+
+          <Anchor p={{ l: "1rem" }} href="https://www.google.com" d="block">
+            Link 1
+          </Anchor>
+          <Anchor p={{ l: "1rem" }} href="https://www.google.com" d="block">
+            Link 2
+          </Anchor>
+          <Anchor p={{ l: "1rem" }} href="https://www.google.com" d="block">
+            Link 3
+          </Anchor>
+        </Row>
+      </Div>
+    )
+  } else
+    return (
+      <Div
+        style={{
+          zIndex: 5000,
+          background: `black`,
+          marginBottom: `1.45rem`,
+          width: "100%",
+          top: 0,
+          position: "fixed",
+        }}
+      >
+        <Row
+          d="flex"
+          align="center"
+          style={{
+            margin: `0 auto`,
+            maxWidth: 1760,
+            padding: `1.45rem 1.0875rem`,
+          }}
+        >
+          <Div p={{ r: "3rem" }}>
+            <h1 style={{ margin: 0 }}>
+              <Link
+                to="/"
+                style={{
+                  color: `black`,
+                  textDecoration: `none`,
+                }}
+              >
+                {siteTitle}
+              </Link>
+            </h1>
+          </Div>
+
+          <Anchor p={{ l: "1rem" }} href="https://www.google.com" d="block">
+            Link 1
+          </Anchor>
+          <Anchor p={{ l: "1rem" }} href="https://www.google.com" d="block">
+            Link 2
+          </Anchor>
+          <Anchor p={{ l: "1rem" }} href="https://www.google.com" d="block">
+            Link 3
+          </Anchor>
+        </Row>
+      </Div>
+    )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,62 @@
 import React from "react"
 import { Link } from "gatsby"
-import { Button } from "atomize"
+import { Div, Container, Row, Anchor, Button } from "atomize"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+import video from "../images/Tag.mp4"
+
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Button bg="warning700">It worked</Button>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  <Div>
+    <Div
+      style={{
+        margin: 0,
+        height: "100vh",
+        width: "100%",
+        overflow: "hidden",
+      }}
+    >
+      <video
+        autoPlay
+        muted
+        loop
+        style={{
+          left: "50%",
+          minHeight: "100%",
+          minWidth: "100%",
+          position: "relative",
+          top: "50%",
+          transform: "translate(-50%, -50%)",
+          overflow: "hidden",
+        }}
+      >
+        <source src={video} type="video/mp4" />
+        <source src={video} type="video/ogg" />
+      </video>
+    </Div>
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <Button bg="warning700">It worked</Button>
+      <Div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </Div>
+      <Link to="/page-2/">Go to page 2</Link>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <Button bg="warning700">It worked</Button>
+      <Div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </Div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  </Div>
 )
 
 export default IndexPage
