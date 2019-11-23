681cb3ac9ee949ea7fa8872df2ad7441ed122f07
Colin Sygiel <c.sygiel@liquidtoolsystems.com>
2019-11-22 19:54:34 -0800

    fixed theme

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,35 @@
 import React from "react"
+
 import { ThemeProvider, StyleReset } from "atomize"
 
-import theme from "constants/theme"
+const theme = {
+  colors: {
+    primary: "#82A9FF",
+    secondary: "#FFCF8A",
+    black: "#0A0A0A",
+    offBlack: "#333333",
+    white: "#FFFFFF",
+  },
+  textSize: {
+    size: {
+      display3: "96px",
+      display2: "60px",
+      display1: "48px",
+      header: "34px",
+      subHeader: "28px",
+      paragraph: "20px",
+      caption: "12px",
+      anchor: "22px",
+    },
+  },
+  fontFamily: {
+    primary: `'Lato'`,
+    secondary: `'Rubik'`,
+  },
+  transition: {
+    custom: "all 0.8s ease",
+  },
+}
 
 export const wrapRootElement = ({ element }) => (
   <ThemeProvider theme={theme}>
