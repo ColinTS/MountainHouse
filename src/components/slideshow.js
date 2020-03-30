1c911b0f07f127c2a0c4bce3e4b229620da9359f
Colin Sygiel <c.sygiel@liquidtoolsystems.com>
2020-03-30 23:04:47 +0100

    testing loading of images in slideshow via url

--- src/components/slideshow.js
+++ src/components/slideshow.js
@@ -5,6 +5,14 @@ import Img from "gatsby-image"
 import Arrow from "../images/arrow.svg"
 import { motion, AnimatePresence } from "framer-motion"
 
+const images = [
+  "https://d33wubrfki0l68.cloudfront.net/dd23708ebc4053551bb33e18b7174e73b6e1710b/dea24/static/images/wallpapers/shared-colors@2x.png",
+  "https://d33wubrfki0l68.cloudfront.net/49de349d12db851952c5556f3c637ca772745316/cfc56/static/images/wallpapers/bridge-02@2x.png",
+  "https://d33wubrfki0l68.cloudfront.net/594de66469079c21fc54c14db0591305a1198dd6/3f4b1/static/images/wallpapers/bridge-01@2x.png",
+]
+
+const imageIndex = Math.floor(Math.random() * 3)
+
 const variants = {
   enter: direction => {
     return {
@@ -74,12 +82,13 @@ export default function SlideShow(props) {
             height: "100%",
           }}
         >
-          <Img
+          <img
             style={{
               borderRadius: 4,
               height: "640px",
             }}
-            fluid={node.childImageSharp.fluid}
+            // fluid={node.childImageSharp.fluid}
+            src={images[page]}
             alt={node.name.replace(/-/g, " ").substring(2)}
           />
         </motion.div>
