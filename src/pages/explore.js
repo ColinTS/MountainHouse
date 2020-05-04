6f774e8871ac907fa0b5887b7511d1df49875512
Colin Sygiel <c.sygiel@liquidtoolsystems.com>
2020-05-04 23:41:58 +0000

    added svg icon to explore markers

--- src/pages/explore.js
+++ src/pages/explore.js
@@ -11,6 +11,7 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import BackgroundImage from "gatsby-background-image"
 import Marker from "../images/location/Marker.svg"
+// import SurfIcon from "../images/explore/SurfIcon.svg"
 
 const isClient = typeof window !== "undefined"
 
@@ -31,6 +32,13 @@ export const data = graphql`
       }
     }
     skatepark: file(relativePath: { eq: "explore/skatepark.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 3000) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    hashpointSurf: file(relativePath: { eq: "explore/panoramaSurf.jpg" }) {
       childImageSharp {
         fluid(maxWidth: 3000) {
           ...GatsbyImageSharpFluid
@@ -91,6 +99,21 @@ const Explore = ({ data }) => {
     setLong(newLong)
   }
 
+  const SurfIcon = ({ width, fill }) => (
+    <svg
+      width={width}
+      height={width}
+      viewBox="0 0 512 512"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M507.351 14.772L505.039 6.962L497.229 4.649C495.779 4.219 461.07 -5.755 406.525 4.903C375.091 11.045 343.219 22.802 311.796 39.847C272.922 60.934 234.6 90.238 197.893 126.944C118.706 206.131 70.216 284.866 43.483 336.972C14.322 393.81 4.061 431.664 3.638 433.247L0 446.883L48.84 463.159L65.116 512L78.753 508.362C80.336 507.939 118.191 497.678 175.028 468.517C227.134 441.783 305.867 393.294 385.055 314.106C421.761 277.4 451.065 239.078 472.153 200.204C489.199 168.78 500.956 136.909 507.096 105.475C517.755 50.932 507.782 16.223 507.351 14.772ZM195.664 423C183.622 430.019 172.41 436.12 162.266 441.344C127.416 459.289 99.815 469.838 84.448 475.109L72.56 439.44L36.891 427.553C42.166 412.183 52.714 384.581 70.657 349.736C75.881 339.593 81.982 328.38 89 316.338H195.664V423ZM225.667 404.591V286.334H107.409C117.094 271.308 128.037 255.419 140.374 238.95V239.01H272.99V371.625H273.05C256.581 383.963 240.692 394.906 225.667 404.591ZM477.242 101.777C471.482 130.015 460.645 158.779 445.033 187.272C425.381 223.133 398.064 258.67 363.84 292.893C343.352 313.381 322.91 331.715 302.992 348.098V209.007H163.902C180.286 189.089 198.619 168.647 219.107 148.159C253.33 113.936 288.867 86.619 324.728 66.967C353.22 51.354 381.984 40.516 410.222 34.758C430.521 30.618 447.651 29.734 460.689 30.094L407.558 83.225L428.773 104.44L481.904 51.309C482.266 64.349 481.382 81.479 477.242 101.777Z"
+        fill={fill}
+      />
+    </svg>
+  )
+
   const Marker = ({ name, newLat, newLong }) => {
     return (
       <div sx={{ position: "absolute", transform: "translate(-50%, -50%)" }}>
@@ -101,16 +124,54 @@ const Explore = ({ data }) => {
           whileHover={{ scale: 1.2 }}
           sx={{
             bg: name === content ? "blue" : "white",
-            width: 35,
-            height: 35,
+            height: 50,
+            width: 50,
             borderStyle: "solid",
             borderColor: "blue",
             boxShadow: "0 0 10px 0px rgba(0, 0, 0, .125)",
             borderRadius: "100%",
             cursor: "pointer",
+            position: "relative",
+
+            // "&::after": {
+            //   content: `''`,
+            //   position: "absolute",
+            //   transform: "translateX(-50%) ",
+            //   top: "96%",
+            //   left: "50%",
+            //   width: 0,
+            //   height: 0,
+            //   borderLeft: "10px solid transparent",
+            //   borderRight: "10px solid transparent",
+            //   borderTop: "10px solid white",
+            //   borderBottomRightRadius: "10px",
+            // },
+            // "&::before": {
+            //   content: `''`,
+            //   position: "absolute",
+            //   transform: "translateX(-50%) ",
+            //   top: "100%",
+            //   left: "50%",
+            //   width: 0,
+            //   height: 0,
+            //   borderLeft: "14px solid transparent",
+            //   borderRight: "14px solid transparent",
+            //   borderTop: "14px solid #428DA4",
+            // },
           }}
           onClick={() => createContent(name, newLat, newLong)}
-        ></motion.div>
+        >
+          <div
+            sx={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+            }}
+          >
+            <SurfIcon width={"24px"} fill={"blue"} />
+          </div>
+        </motion.div>
       </div>
     )
   }
@@ -222,6 +283,7 @@ const Explore = ({ data }) => {
           )}
         </div>
       </div>
+      <SurfIcon width={"32px"} />
     </Layout>
   )
 }
